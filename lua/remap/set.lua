d2647f4c62c880ed271fa503c309067bb79d1871
Graham Clifford <gjcliff@gmail.com>
2024-08-14 09:05:07 -0500

    Added obsidian notes plugin

--- lua/remap/set.lua
+++ lua/remap/set.lua
@@ -2,6 +2,8 @@
 package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/magick/init.lua;"
 -- package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
 
+vim.opt.conceallevel = 1
+
 vim.opt.spell = false
 vim.opt.nu = true
 vim.opt.relativenumber = true
