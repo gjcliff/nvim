bc31b68bd3ee219b8205870de62b5968091c1fe1
Graham Clifford <gclifford@modalic.com>
2025-10-11 20:11:40 -0400

    fixed which-key dependency issue

--- lua/graham/lazy/platformio.lua
+++ lua/graham/lazy/platformio.lua
@@ -1,20 +1,21 @@
 return {
-  'anurag3301/nvim-platformio.lua',
+  "anurag3301/nvim-platformio.lua",
   -- cmd = { 'Pioinit', 'Piorun', 'Piocmdh', 'Piocmdf', 'Piolib', 'Piomon', 'Piodebug', 'Piodb' },
 
   -- dependencies are always lazy-loaded unless specified otherwise
   dependencies = {
-    { 'akinsho/toggleterm.nvim' },
-    { 'nvim-telescope/telescope.nvim' },
-    { 'nvim-telescope/telescope-ui-select.nvim' },
-    { 'nvim-lua/plenary.nvim' },
+    { "akinsho/toggleterm.nvim" },
+    { "nvim-telescope/telescope.nvim" },
+    { "nvim-telescope/telescope-ui-select.nvim" },
+    { "nvim-lua/plenary.nvim" },
+    { "folke/which-key.nvim" }, -- Add this
   },
   config = function()
-    require('platformio').setup({
-      lsp = 'clangd', --default: ccls, other option: clangd
+    require("platformio").setup({
+      lsp = "clangd", --default: ccls, other option: clangd
       -- If you pick clangd, it also creates compile_commands.json
 
-      menu_key = '<leader>h',
+      menu_key = "<leader>h",
     })
-  end
+  end,
 }
