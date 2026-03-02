c32badfd08bf3f984a882726bf46178a77b89164
Graham Clifford <gjcliff@gmail.com>
2026-03-02 09:07:10 -0500

    formatting changes

--- lua/graham/lazy/treesitter.lua
+++ lua/graham/lazy/treesitter.lua
@@ -3,12 +3,21 @@ return {
   run = ":TSUpdate",
   config = function()
     require("nvim-treesitter").setup({
-      ensure_installed = { "cpp", "python", "c", "lua", "vim", "vimdoc", "query", "norg" },
+      ensure_installed = {
+        "cpp",
+        "python",
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "norg",
+      },
       sync_install = false,
       auto_install = true,
       highlight = {
         enable = true,
       },
     })
-  end
+  end,
 }
