ca0e27a32286dfb3549df88766c72305a8dfe154
Graham Clifford <gjcliff@gmail.com>
2026-03-04 22:59:10 -0500

    added oil-git

--- lua/graham/lazy/oil.lua
+++ lua/graham/lazy/oil.lua
@@ -1,3 +1,5 @@
+local detail = false
+
 return {
   "stevearc/oil.nvim",
   ---@module 'oil'
@@ -69,24 +71,61 @@ return {
       keymaps = {
         ["g?"] = "actions.show_help",
         ["<CR>"] = "actions.select",
-        ["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
-        ["<C-a>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
-        ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+        ["<C-s>"] = {
+          "actions.select",
+          opts = { vertical = true },
+          desc = "Open the entry in a vertical split",
+        },
+        ["<C-a>"] = {
+          "actions.select",
+          opts = { horizontal = true },
+          desc = "Open the entry in a horizontal split",
+        },
+        ["<C-t>"] = {
+          "actions.select",
+          opts = { tab = true },
+          desc = "Open the entry in new tab",
+        },
         ["<C-p>"] = "actions.preview",
         ["<C-c>"] = "actions.close",
         ["<C-r>"] = "actions.refresh",
         -- ["-"] = "actions.parent",
         ["_"] = "actions.open_cwd",
         ["`"] = "actions.cd",
-        ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
+        ["~"] = {
+          "actions.cd",
+          opts = { scope = "tab" },
+          desc = ":tcd to the current oil directory",
+        },
         ["gs"] = "actions.change_sort",
         ["gx"] = "actions.open_external",
         ["g."] = "actions.toggle_hidden",
         ["g\\"] = "actions.toggle_trash",
+        ["gd"] = {
+          desc = "Toggle file detail view",
+          callback = function()
+            detail = not detail
+            if detail then
+              require("oil").set_columns({
+                "icon",
+                "permissions",
+                "size",
+                "mtime",
+              })
+            else
+              require("oil").set_columns({ "icon" })
+            end
+          end,
+        },
       },
 
       -- custom
-      vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open parent directory" }),
+      vim.keymap.set(
+        "n",
+        "-",
+        "<cmd>Oil<CR>",
+        { desc = "Open parent directory" }
+      ),
 
       -- Set to false to disable all of the above keymaps
       use_default_keymaps = false,
@@ -194,5 +233,5 @@ return {
         border = "rounded",
       },
     })
-  end
+  end,
 }
